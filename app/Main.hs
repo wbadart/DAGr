ce8c2cca8e8dc60a29188ac871e1dd6ff14ea39f
Will Badart <badart_william@bah.com>
2020-06-22 15:24:55 -0700

    Brittany-formatted Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -6,11 +6,11 @@
 module Main where
 
 import qualified Codec.Binary.UTF8.String      as UTF8
-import Data.Aeson (eitherDecode)
+import           Data.Aeson                     ( eitherDecode )
 import qualified Data.ByteString.Lazy          as BS
-import Language.Python.Common.Pretty (prettyText)
+import           Language.Python.Common.Pretty  ( prettyText )
 
-import Language.Python.JSONComposer
+import           Language.Python.JSONComposer
 
 main :: IO ()
 main =

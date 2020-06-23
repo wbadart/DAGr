3104ba9824b4d577ca372225e2b36470824df3c7
Will Badart <badart_william@bah.com>
2020-06-23 15:50:43 -0700

    Added test suite
    With initial golden test case. It uses a different model so it should be
    failing.

---
+++ tests/Main.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE TypeApplications #-}
+
+module Main where
+
+import           Data.Aeson                     ( decode )
+import qualified Codec.Binary.UTF8.String      as UTF8
+import qualified Data.ByteString.Lazy          as BS
+import           Test.Tasty                     ( TestTree
+                                                , defaultMain
+                                                , testGroup
+                                                )
+import           Test.Tasty.Golden              ( findByExtension
+                                                , goldenVsString
+                                                )
+import           System.FilePath                ( takeBaseName
+                                                , replaceExtension
+                                                )
+
+import           Language.Python.Common         ( prettyText )
+
+import           Language.Python.JSONComposer
+
+main :: IO ()
+main = goldenTests >>= defaultMain
+
+goldenTests :: IO TestTree
+goldenTests = do
+  jsonFiles <- findByExtension [".json"] "./tests"
+  return $ testGroup
+    "JSONComposer Golden Tests"
+    [ goldenVsString (takeBaseName jsonFile)
+                     pyFile
+                     (BS.readFile jsonFile >>= pipeline)
+    | jsonFile <- jsonFiles
+    , let pyFile = replaceExtension jsonFile ".py"
+    ]
+
+pipeline :: BS.ByteString -> IO BS.ByteString
+pipeline contents =
+  let json = decode @JSONPyComposition contents
+      py   = fmap prettyText . parse <$> json
+  in  pure $ maybe
+        (error "JSON decode failed")
+        (either (error "JSONComposer parse failed") (BS.pack . UTF8.encode))
+        py

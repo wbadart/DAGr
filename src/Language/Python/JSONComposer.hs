bedd5aa617e8d07f6301865d149470a20b3cc931
Will Badart <badart_william@bah.com>
2020-06-24 01:09:04 -0700

    Fix input ordering with the label hack idea

--- src/Language/Python/JSONComposer.hs
+++ src/Language/Python/JSONComposer.hs
@@ -22,6 +22,7 @@ import           Data.Aeson                     ( FromJSON(..) )
 import           Data.Graph.Inductive    hiding ( nodes
                                                 , edges
                                                 )
+import           Data.List                      ( sortOn )
 import           Data.Map.Strict                ( Map )
 import qualified Data.Map.Strict               as M
 import           Data.Maybe                     ( fromJust )
@@ -42,13 +43,13 @@ parse j =
       assignmentOrder = topsort g
       mkAssignment ctx = do
         let NodeLabel { identifier, value } = lab' ctx
-            inputs                          = pre' ctx
+            inputs                          = lpre' ctx
         (lhs, _) <- parseExpr identifier ""
         (rhs, _) <- parseExpr value ""
         if null inputs
           then return (Assign [lhs] rhs SpanEmpty)
           else do
-            exprs <- traverse (parseArgs g) inputs
+            exprs <- traverse (parseArgs g . fst) (sortOn snd inputs)
             let args = (`ArgExpr` SpanEmpty) . fst <$> exprs
             return (Assign [lhs] (Call rhs args SpanEmpty) SpanEmpty)
   in  Module <$> traverse (mkAssignment . context g) assignmentOrder
@@ -59,13 +60,13 @@ data NodeLabel = NodeLabel
   , value      :: String
   } deriving (Eq, Ord, Show)
 
-toGraph :: JSONPyComposition -> Gr NodeLabel ()
+toGraph :: JSONPyComposition -> Gr NodeLabel Int
 toGraph JSONPyComposition { nodes, edges } =
   let nodeId = (M.!) (reverseMap $ M.fromList $ zip [0 ..] $ M.keys nodes)
       labeledEdges =
-          [ (nodeId src, nodeId dst, ())
+          [ (nodeId src, nodeId dst, i)
           | (dst, srcs) <- M.toList edges
-          , src         <- srcs
+          , (i, src)    <- zip [0 .. ] srcs
           ]
       labeledNodes = zip [0 ..] (uncurry NodeLabel <$> M.toList nodes)
   in  insEdges labeledEdges $ insNodes labeledNodes empty

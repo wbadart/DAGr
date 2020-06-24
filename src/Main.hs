927b159bf8a720611619dc96167a849eb03208fb
Will Badart <badart_william@bah.com>
2020-06-24 12:36:41 -0700

    cabal init + stack init
    I think nix was trying to build ghc so I'm just going to stick to
    regular stack for this one. Keeping shell.nix for the dev environment.

---
+++ src/Main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+
+module Main where
+
+import           Language.Python.JSONComposer 
+import           Yesod
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
+main :: IO ()
+main = warp 3000 HelloWorld

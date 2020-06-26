181151527269e06ee033f928f4e9de389db2d2c9
Will Badart <badart_william@bah.com>
2020-06-26 15:49:46 -0700

    Got app to put rectangles down at the "mouse position"
    It's a bit of a hack since I currently have to account for the svg
    element's offset (mouse position is relative to the window, but the x
    and y of each rect must be relative to the svg corner).

--- app/src/elm/Main.elm
+++ app/src/elm/Main.elm
@@ -1,12 +1,19 @@
 port module Main exposing (main)
 
 import Browser
+import Browser.Events as E
 import Dict exposing ( Dict )
 import Maybe exposing ( Maybe )
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
--- import Json.Decode as D
+import Svg exposing ( Svg, svg )
+import Svg.Attributes as A
+import Json.Decode as D
+
+-- ==========
+-- Page setup
+-- ==========
 
 port setupReceiver : (String -> msg) -> Sub msg
 port teardownReceiver : (String -> msg) -> Sub msg
@@ -14,69 +21,130 @@ port teardownReceiver : (String -> msg) -> Sub msg
 main : Program () Model Msg
 main =
   Browser.element
-    { init = \_ -> ( { setup = "", teardown = "", graph = Dict.empty }, Cmd.none )
+    { init = \_ -> ( init, Cmd.none )
     , view = view
     , update = update
-    , subscriptions = \_ -> Sub.batch [ setupReceiver EditSetup, teardownReceiver EditTeardown ]
+    , subscriptions = subscriptions
     }
 
+init : Model
+init =
+  { mousePosition = ( 0, 0 )
+  , program =
+    { setup = ""
+    , teardown = ""
+    , graph =
+        { nodes = Dict.empty
+        , edges = Dict.empty
+        }
+    }
+  }
+
+subscriptions : model -> Sub Msg
+subscriptions _ =
+  Sub.batch
+    [ setupReceiver EditSetup
+    , teardownReceiver EditTeardown
+    , E.onMouseMove (D.map2 MouseMove (D.field "pageX" D.float) (D.field "pageY" D.float))
+    ]
+
+
+-- ==========
+-- Application model
+-- ==========
+
+
 type alias Model =
-  { setup : String
-  , teardown : String
-  , graph : Dict Int Node
+  { mousePosition : ( Float, Float )
+  , program :
+    { setup : String
+    , teardown : String
+    , graph : Graph
+    }
   }
 
 type alias Graph =
-  { nodes : Dict String String
-  , edges : Dict String (List String)
+  { nodes : Dict Int Node
+  , edges : Dict Int ( List Int )
   }
 
 type alias Node =
   { name : String
   , expr : String
+  , pos : ( Float, Float )
   }
+
 
 type Msg =
     NewNode
   | EditNodeName Int String
   | EditNodeExpr Int String
+
   | EditSetup String
   | EditTeardown String
 
+  | MouseMove Float Float
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
+  let prog = model.program
+  in
   case msg of
     NewNode ->
-      ( { model | graph = Dict.insert ( Dict.size model.graph ) { name = "", expr = "" } model.graph }
+      let updatedNodes =
+            Dict.insert
+              ( Dict.size prog.graph.nodes )
+              { name = "", expr = "", pos = model.mousePosition }
+              prog.graph.nodes
+          g = prog.graph
+      in
+      ( { model | program = { prog | graph = { g | nodes = updatedNodes } } }
       , Cmd.none
       )
 
     EditNodeName i s ->
-      ( { model | graph = Dict.update i ( Maybe.map (\n -> {n | name = s}) ) model.graph }
+      let updatedNodes = Dict.update i ( Maybe.map (\n -> { n | name = s }) ) prog.graph.nodes
+          g = prog.graph
+      in
+      ( { model | program = { prog | graph = { g | nodes = updatedNodes } } }
       , Cmd.none
       )
 
     EditNodeExpr i s ->
-      ( { model | graph = Dict.update i ( Maybe.map (\n -> {n | expr = s}) ) model.graph }
+      let updatedNodes = Dict.update i ( Maybe.map (\n -> { n | expr = s }) ) prog.graph.nodes
+          g = prog.graph
+      in
+      ( { model | program = { prog | graph = { g | nodes = updatedNodes } } }
       , Cmd.none
       )
 
-    EditSetup    s -> ( { model | setup    = s }, Cmd.none )
-    EditTeardown s -> ( { model | teardown = s }, Cmd.none )
+    EditSetup    s -> ( { model | program = { prog | setup    = s } }, Cmd.none )
+    EditTeardown s -> ( { model | program = { prog | teardown = s } }, Cmd.none )
+
+    MouseMove x y -> ( { model | mousePosition = ( x, y ) }, Cmd.none )
+
+
+-- ==========
+-- Rendering
+-- ==========
+
 
 view : Model -> Html Msg
 view model =
-  div [ class "app", onClick NewNode ]
-    [ div []
-        [ text ( "num nodes: " ++ ( String.fromInt ( Dict.size model.graph ) ) )
-        ]
-    , div [] ( List.map viewNode ( Dict.toList ( model.graph ) ) )
-    ]
+  svg
+    [ A.width "100%", A.height "100%", onClick NewNode ]
+    ( List.map viewNode ( Dict.toList model.program.graph.nodes ) )
 
-viewNode : ( Int, Node ) -> Html Msg
-viewNode (i, node) =
-  p []
-    [ text "node:"
-    , input [ value node.name, onInput ( EditNodeName i ) ] []
-    , input [ value node.expr ] []
+viewNode : ( Int, Node ) -> Svg Msg
+viewNode ( i, node ) =
+  let ( x, y ) = node.pos
+  in
+  Svg.rect
+    [ A.x ( ( String.fromFloat ( x - 300 ) ) ++ "px" )
+    , A.y ( ( String.fromFloat ( y ) ) ++ "px" )
+    , A.width "100"
+    , A.height "100"
+    ]
+    [ Svg.text node.name
     ]
